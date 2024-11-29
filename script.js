aea238aa1e8c97c7a5626dfd2bd1a27a678577d8
Littzy <littzypb@gmail.com>
2024-11-29 12:09:29 -0600

    More

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let cart = document.querySelector('.cart'); 
 let closeCart= document.querySelector('.close'); 
 let body = document.querySelector('body'); 
-let menuCart = document.querySelector()
 
+let listProductHTML = querySelectto('.cartList')
 cart.addEventListener('click', () =>{
      body.classList.toggle('showCart')
 })
@@ -11,3 +11,18 @@ cart.addEventListener('click', () =>{
 closeCart.addEventListener('click', () =>{
     body.classList.toggle('showCart')
 })
+
+let listProducts =[]; 
+
+const initApp = () =>
+    {
+        // fetch gets data from json file
+        fetch('products.json')
+        then(response => response.jason())
+        then(data=>{
+            listProducts = data; 
+            console.log();
+            
+        })
+    }
+    initApp(); 
